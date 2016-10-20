7631b0c751355b1f570c2f7d141e749c832a20bc
Gilmar rodrigues <gilmar_rodrigues11@hotmail.com>
2016-10-20 11:09:25 -0300

    alterei o alergia.gradle(build):gradle:2.2.0 e mudei a versão do gradle para 3.1. A velocidade ficou melhor 100%.

--- app/src/main/java/br/com/alimentar/alergia/activity/MainActivity.java
+++ app/src/main/java/br/com/alimentar/alergia/activity/MainActivity.java
@@ -3,7 +3,6 @@ package br.com.alimentar.alergia.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -15,9 +14,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
