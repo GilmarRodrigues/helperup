091748cd1df7a5ae4615b22a4bceb3fe4484e781
Gilmar rodrigues <gilmar_rodrigues11@hotmail.com>
2016-12-07 15:17:56 -0300

    classe UpdateAlergenicosDialog pornta! o usuario pode editar seus alergenicos

--- app/src/main/java/br/com/alimentar/alergia/activity/ProfileActivity.java
+++ app/src/main/java/br/com/alimentar/alergia/activity/ProfileActivity.java
@@ -1,22 +1,17 @@
 package br.com.alimentar.alergia.activity;
 
-import android.app.Dialog;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,21 +24,20 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import br.com.alimentar.alergia.R;
 import br.com.alimentar.alergia.fragment.CustomBottomSheetDialogFragment;
+import br.com.alimentar.alergia.fragment.dialog.UpdateAlergenicosDialog;
 import br.com.alimentar.alergia.fragment.dialog.UpdateUserDialog;
+import br.com.alimentar.alergia.model.Substancia;
 import br.com.alimentar.alergia.model.Tabelas;
 import br.com.alimentar.alergia.model.User;
-import br.com.alimentar.alergia.view.RoundedImageView;
 import id.zelory.compressor.Compressor;
 import id.zelory.compressor.FileUtil;
-
-import static android.R.attr.key;
-import static br.com.alimentar.alergia.R.id.iv_produto;
-import static br.com.alimentar.alergia.R.id.up;
 
 public class ProfileActivity extends BaseActivity {
     private DatabaseReference mDatabaseUser;
@@ -56,6 +50,7 @@ public class ProfileActivity extends BaseActivity {
     private ImageView iv_perfil;
     private Uri mImagemUri = null;
     private String key;
+    private LinearLayout mLLAlergenicos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,10 +65,20 @@ public class ProfileActivity extends BaseActivity {
         key = mAuth.getCurrentUser().getUid();
 
         findViewById(R.id.iv_editar_perfil).setOnClickListener(onClickUpdateUser());
+        findViewById(R.id.iv_editar_alergenicos).setOnClickListener(onClickUpdateAlergenicos());
 
         setUser();
 
         iv_perfil.setOnClickListener(onClickUpdateImageUser());
+    }
+
+    private View.OnClickListener onClickUpdateAlergenicos() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateAlergenicos();
+            }
+        };
     }
 
     private View.OnClickListener onClickUpdateImageUser() {
@@ -100,6 +105,7 @@ public class ProfileActivity extends BaseActivity {
         tv_email = (TextView) findViewById(R.id.tv_email);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         iv_perfil = (ImageView) findViewById(R.id.iv_perfil);
+        mLLAlergenicos = (LinearLayout) findViewById(R.id.llt_alergenicos);
 
         mDatabaseUser.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -113,6 +119,16 @@ public class ProfileActivity extends BaseActivity {
                 } else {
                     progressBar.setVisibility(View.INVISIBLE);
                     Log.i(TAG, "Foto: " + mUser.imagem);
+                }
+
+                List<Substancia> substancias = mUser.substancias;
+                for (int i=0; i <substancias.size(); i++) {
+                    if (substancias.get(i).status.equals(getString(R.string.const_contem))) {
+                        TextView tv_alergenico = new TextView(ProfileActivity.this);
+                        tv_alergenico.setPadding(0, 0, 0, 15);
+                        tv_alergenico.setText(substancias.get(i).nome);
+                        mLLAlergenicos.addView(tv_alergenico);
+                    }
                 }
 
             }
@@ -189,6 +205,23 @@ public class ProfileActivity extends BaseActivity {
                     }
                 } else {
                     Snackbar.make(v, getString(R.string.msg_imagem_null), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        });
+    }
+
+    private void updateAlergenicos() {
+        UpdateAlergenicosDialog.show(getSupportFragmentManager(), mUser, new UpdateAlergenicosDialog.Callback() {
+            @Override
+            public void onAlergenicosUpdate(List<Substancia> alergenicos) {
+                mLLAlergenicos.removeAllViews();
+                for (int i=0; i <alergenicos.size(); i++) {
+                    if (alergenicos.get(i).status.equals(getString(R.string.const_contem))) {
+                        TextView tv_alergenico = new TextView(ProfileActivity.this);
+                        tv_alergenico.setPadding(0, 0, 0, 15);
+                        tv_alergenico.setText(alergenicos.get(i).nome);
+                        mLLAlergenicos.addView(tv_alergenico);
+                    }
                 }
             }
         });
---
+++ app/src/main/java/br/com/alimentar/alergia/fragment/dialog/UpdateAlergenicosDialog.java
@@ -0,0 +1,150 @@
+package br.com.alimentar.alergia.fragment.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.com.alimentar.alergia.R;
+import br.com.alimentar.alergia.adapter.SubstanciasAdapter;
+import br.com.alimentar.alergia.model.Substancia;
+import br.com.alimentar.alergia.model.Tabelas;
+import br.com.alimentar.alergia.model.User;
+
+/**
+ * Created by gilmar on 07/12/16.
+ */
+
+public class UpdateAlergenicosDialog extends BaseDialog {
+    private Callback callback;
+    private User mUser;
+    private DatabaseReference mDatabaseUser;
+    private FirebaseAuth mAuth;
+    private ListView mListView;
+
+    public static void show(FragmentManager fm, User user, Callback callback) {
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag("update_alergenicos");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        UpdateAlergenicosDialog frag = new UpdateAlergenicosDialog();
+        frag.callback = callback;
+        Bundle args = new Bundle();
+        args.putParcelable(User.KEY, user);
+        frag.setArguments(args);
+        frag.show(fm, "update_alergenicos");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_update_alergenicos, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child(Tabelas.USUARIO);
+        mDatabaseUser.keepSynced(true);
+
+        mUser = getArguments().getParcelable(User.KEY);
+
+        mListView = (ListView) view.findViewById(R.id.list_view);
+        mListView.setAdapter(new SubstanciasAdapter(getActivity(), onClickSwitch(), mUser.substancias));
+
+        //campo_nome = (TextView) view.findViewById(edit_nome);
+        if (mUser != null) {
+            //campo_nome.setText(mUser.nome);
+        }
+
+        view.findViewById(R.id.btn_salvar).setOnClickListener(onClickSalvar());
+        view.findViewById(R.id.btn_cancelar).setOnClickListener(onClickCancelar());
+
+        return view;
+    }
+
+    private SubstanciasAdapter.OnClickSwitch onClickSwitch() {
+        return new SubstanciasAdapter.OnClickSwitch() {
+            @Override
+            public void onClick(View view, int position) {
+                if (mUser.substancias.get(position).status.equals(getString(R.string.const_nao_contem))) {
+                    mUser.substancias.get(position).status = getString(R.string.const_contem);
+                } else {
+                    mUser.substancias.get(position).status = getString(R.string.const_nao_contem);
+                }
+            }
+        };
+    }
+
+
+    private View.OnClickListener onClickCancelar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        };
+    }
+
+    private View.OnClickListener onClickSalvar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String key = mAuth.getCurrentUser().getUid();
+
+                if (mUser.substancias.size() > 0) {
+
+                    showProgressDialog(R.string.msg_update);
+
+                    mDatabaseUser.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.hasChild(key)) {
+                                User user = new User(mUser.nome, mUser.email, mUser.imagem, mUser.substancias);
+                                Map<String, Object> postValue = user.toMap();
+                                Map<String, Object> childUpdates = new HashMap<String, Object>();
+                                childUpdates.put(key, postValue);
+                                mDatabaseUser.updateChildren(childUpdates);
+
+
+                                hideProgressDialog();
+                            }
+                            dismiss();
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    if (callback != null) {
+                        callback.onAlergenicosUpdate(mUser.substancias);
+                    }
+                }
+            }
+        };
+    }
+
+    public static interface Callback {
+        public void onAlergenicosUpdate(List<Substancia> alergenicos);
+    }
+}
