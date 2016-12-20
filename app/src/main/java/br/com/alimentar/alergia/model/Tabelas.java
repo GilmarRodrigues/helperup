2894776d3604e6aaed1ac1c9a1d88152f01c7099
Gilmar rodrigues <gilmar_rodrigues11@hotmail.com>
2016-12-20 20:37:18 -0300

    alterar na categoria massa para massa. Incluir na categoria salgadinhos. incluir na alergenicos: amendoim e cevado. ordenar categorias e alergenicos

--- app/src/main/java/br/com/alimentar/alergia/model/Tabelas.java
+++ app/src/main/java/br/com/alimentar/alergia/model/Tabelas.java
@@ -25,10 +25,10 @@ public class Tabelas {
     public static final String APP_PACKAGER_NAME = "br.com.alimentar.alergia";
 
     public static List<Substancia> addSubstancias(Context context) {
-        String[] substancias = {context.getString(R.string.substancia_leite), context.getString(R.string.substancia_ovo),
-                context.getString(R.string.substancia_oleoginosas), context.getString(R.string.substancia_crustaceos),
-                context.getString(R.string.substancia_soja), context.getString(R.string.substancia_trigo),
-                context.getString(R.string.substancia_peixe), context.getString(R.string.substancia_gluten)};
+        String[] substancias = {context.getString(R.string.substancia_amendorim), context.getString(R.string.substancia_cevada), context.getString(R.string.substancia_crustaceos),
+                context.getString(R.string.substancia_leite), context.getString(R.string.substancia_gluten), context.getString(R.string.substancia_ovo),
+                context.getString(R.string.substancia_oleoginosas), context.getString(R.string.substancia_peixe), context.getString(R.string.substancia_soja),
+                context.getString(R.string.substancia_trigo)};
         List<Substancia> list = new ArrayList<>();
         for (int i = 0; i < substancias.length; i++) {
             list.add(new Substancia(substancias[i], context.getString(R.string.const_nao_contem)));
@@ -37,12 +37,11 @@ public class Tabelas {
     }
 
     public static String[] addCategorias(Context context) {
-        String[] categorias = new String[]{context.getString(R.string.categoria_selecione_item), context.getString(R.string.categoria_bebidas),
-                context.getString(R.string.categoria_biscoitos), context.getString(R.string.categoria_congelados), context.getString(R.string.categoria_doces_sobremesas),
-                context.getString(R.string.categoria_enlatados), context.getString(R.string.categoria_graos_cereais), context.getString(R.string.categoria_leite),
-                context.getString(R.string.categoria_iorgute), context.getString(R.string.categoria_massa),context.getString(R.string.categoria_paes),
-                context.getString(R.string.categoria_bolos), context.getString(R.string.categoria_queijo_frios), context.getString(R.string.categoria_salgadinhos),
-                context.getString(R.string.categoria_outros)};
+        String[] categorias = new String[]{context.getString(R.string.categoria_selecione_item), context.getString(R.string.categoria_bebidas), context.getString(R.string.categoria_biscoitos),
+                context.getString(R.string.categoria_bolos), context.getString(R.string.categoria_congelados), context.getString(R.string.categoria_doces_sobremesas),
+                context.getString(R.string.categoria_enlatados), context.getString(R.string.categoria_graos_cereais), context.getString(R.string.categoria_iorgute),
+                context.getString(R.string.categoria_leite), context.getString(R.string.categoria_massa),context.getString(R.string.categoria_paes),
+                context.getString(R.string.categoria_queijo_frios), context.getString(R.string.categoria_salgadinhos), context.getString(R.string.categoria_outros)};
         return categorias;
     }
 
