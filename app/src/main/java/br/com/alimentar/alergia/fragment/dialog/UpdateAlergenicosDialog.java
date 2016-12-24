eb3db374e2e14c54f770662af984d62f68e63a87
Gilmar rodrigues <gilmar_rodrigues11@hotmail.com>
2016-12-24 18:16:30 -0300

    dialog: colocar titulo nos dialogos. versao > 21

--- app/src/main/java/br/com/alimentar/alergia/fragment/dialog/UpdateAlergenicosDialog.java
+++ app/src/main/java/br/com/alimentar/alergia/fragment/dialog/UpdateAlergenicosDialog.java
@@ -1,6 +1,8 @@
 package br.com.alimentar.alergia.fragment.dialog;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -75,6 +77,7 @@ public class UpdateAlergenicosDialog extends BaseDialog {
 
         return view;
     }
+
 
     private SubstanciasAdapter.OnClickSwitch onClickSwitch() {
         return new SubstanciasAdapter.OnClickSwitch() {
--- app/src/main/java/br/com/alimentar/alergia/fragment/dialog/UpdateUserDialog.java
+++ app/src/main/java/br/com/alimentar/alergia/fragment/dialog/UpdateUserDialog.java
@@ -1,6 +1,8 @@
 package br.com.alimentar.alergia.fragment.dialog;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
